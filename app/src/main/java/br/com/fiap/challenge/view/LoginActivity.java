ab930a3f02910b7e0cfc80fde600789bd124463b
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-11 14:38:24 -0300

    Adicionando layouts da tela de login - Leonardo

--- app/src/main/java/br/com/fiap/challenge/view/LoginActivity.java
+++ app/src/main/java/br/com/fiap/challenge/view/LoginActivity.java
@@ -2,6 +2,7 @@ package br.com.fiap.challenge.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import br.com.fiap.challenge.R;
 
@@ -11,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void Cadastrar(View view) {
+    }
+
+    public void RecuperarSenha(View view) {
     }
 }
